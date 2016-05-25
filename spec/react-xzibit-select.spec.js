85628c4fed8463fdb047f064a0d3e711165d8db3
Stephen Smith <stephensmith@Stephens-MacBook-Pro.local>
2016-05-25 16:25:00 -0400

    fixing lint

--- spec/react-xzibit-select.spec.js
+++ spec/react-xzibit-select.spec.js
@@ -3,30 +3,30 @@
 //var ReactXzibitSelect = require('../lib/react-xzibit-select.jsx');
 
 
-describe("ReactXzibitSelect", function() {
+describe('ReactXzibitSelect', function() {
   // var component;
 
   // var exampleOptions = [
-  //   {value: "Apple", label: "Apple", color: "red", growsOn: "tree"},
-  //   {value: "Blueberry", label: "Blueberry", color: "blue", growsOn: "bush"}
+  //   {value: 'Apple', label: 'Apple', color: 'red', growsOn: 'tree'},
+  //   {value: 'Blueberry', label: 'Blueberry', color: 'blue', growsOn: 'bush'}
   // ];
   // var exampleFilterDimensions = [
-  //   {name: "Color", key: "color", options: [{value: "red", label: "red"},{value: "green", label: "green"}, {value: "yellow", label: "yellow"}, {value: "blue", label: "blue"}]}, 
-  //   {name: "Grows On", key: "growsOn", options: [{value: "tree", label: "tree"}, {value: "bush", label: "bush"}]}
+  //   {name: 'Color', key: 'color', options: [{value: 'red', label: 'red'},{value: 'green', label: 'green'}, {value: 'yellow', label: 'yellow'}, {value: 'blue', label: 'blue'}]},
+  //   {name: 'Grows On', key: 'growsOn', options: [{value: 'tree', label: 'tree'}, {value: 'bush', label: 'bush'}]}
   // ];
   // var onChangeFunc = function() { };
 
   //   beforeEach(function() {
   //     component = TestUtils.renderIntoDocument(
-  //       <ReactXzibitSelect 
+  //       <ReactXzibitSelect
   //       	options={exampleOptions}
-  //       	values={["Apple"]}
+  //       	values={['Apple']}
   //       	filterDimensions={exampleFilterDimensions}
   //         onChange={onChangeFunc}/>
   //     );
   //   });
 
-    it("should render", function() {
+    it('should render', function() {
       expect(1).toEqual(1);
     });
 });
