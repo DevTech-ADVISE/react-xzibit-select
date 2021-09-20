f8d9f1eae47a7f2e05d19d7c83fcf4b85d9a479b
Xavier Berry <bobby.xavier.berry@gmail.com>
2021-09-20 16:31:00 -0400

    Check parameter inheritance

--- src/components/option-list.js
+++ src/components/option-list.js
@@ -1,39 +1,44 @@
-var React = require('react');
-var createReactClass = require('create-react-class');
-var types = require('prop-types');
-var OptionListItem = require('./option-list-item');
-var LazyRender = require('react-lazy-render');
-var ReactSizeBox = require('react-sizebox');
-var classes = require('classnames');
+var React = require("react");
+var createReactClass = require("create-react-class");
+var types = require("prop-types");
+var OptionListItem = require("./option-list-item");
+var LazyRender = require("react-lazy-render");
+var ReactSizeBox = require("react-sizebox");
+var classes = require("classnames");
 
 var OptionList = createReactClass({
   propTypes: {
     options: types.array,
     onClick: types.func,
     addAll: types.bool,
-    addAllFunc: types.func
+    addAllFunc: types.func,
+    openTipOptions: types.object,
   },
-  buildOption: function(opt, index){
-    if(opt.addAll) {
+  buildOption: function (opt, index) {
+    if (opt.addAll) {
       return (
         <OptionListItem
-          key='Add All'
+          key="Add All"
           addAll={this.props.addAll}
           onClick={this.props.addAllFunc}
-          value={'Add All'}
-          label='Add All' />
+          value={"Add All"}
+          label="Add All"
+        />
       );
     }
 
-    var toolTipContent = '', toolTipTitle = '', label;
-    if(opt.toolTipContent) toolTipContent = String(opt.toolTipContent);
-    if(opt.toolTipTitle) toolTipTitle = String(opt.toolTipTitle);
-    if(opt.labelComponent)
-      label = opt.labelComponent;
-    else
-      label = opt.label;
+    var toolTipContent = "",
+      toolTipTitle = "",
+      label;
+    if (opt.toolTipContent) toolTipContent = String(opt.toolTipContent);
+    if (opt.toolTipTitle) toolTipTitle = String(opt.toolTipTitle);
+    if (opt.labelComponent) label = opt.labelComponent;
+    else label = opt.label;
 
-    var className = classes({'rxs-item-even': index % 2 === 0, 'rxs-item-odd': index % 2 === 1});
+    var className = classes({
+      "rxs-item-even": index % 2 === 0,
+      "rxs-item-odd": index % 2 === 1,
+    });
 
     return (
       <OptionListItem
@@ -45,29 +50,30 @@ var OptionList = createReactClass({
         toolTipContent={toolTipContent}
         toolTipTitle={toolTipTitle}
         onMobileTooltip={this.props.onMobileTooltip}
-        openTipOptions={this.props.openTipOptions} />
+        openTipOptions={this.props.openTipOptions}
+      />
     );
-
   },
-  render: function() {
-    if(this.props.addAll) {
-      this.props.options.unshift({addAll: true});
+  render: function () {
+    if (this.props.addAll) {
+      this.props.options.unshift({ addAll: true });
     }
 
     return (
-      <div className='content'>
-        <ReactSizeBox className='overflow-y rsx-SizeBox' heightProp='maxHeight'>
+      <div className="content">
+        <ReactSizeBox className="overflow-y rsx-SizeBox" heightProp="maxHeight">
           <LazyRender
-            className='rxs-option-list rsx-lazyRender'
+            className="rxs-option-list rsx-lazyRender"
             generatorData={this.props.options}
             generatorFunction={this.buildOption}
-            maxHeight={400}>
+            maxHeight={400}
+          >
             <li>None Found</li>
           </LazyRender>
         </ReactSizeBox>
       </div>
     );
-  }
+  },
 });
 
 module.exports = OptionList;
